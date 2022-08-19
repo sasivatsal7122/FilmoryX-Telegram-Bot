3dcc1c832f8d5ddd21bd905b8df62863e40873b7
SasiVatsal <71326149+sasivatsal7122@users.noreply.github.com>
2022-08-19 14:02:18 +0530

    changed  get torrent

--- get_torrent.py
+++ get_torrent.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup as soup
 
 
 def get_torrent(movie_name):
-    
-    movie = movie_name.replace(" ","-")
+    movie = movie_name.replace("[","")
+    movie = movie.replace("]","")
+    movie = movie.replace(" ","-")
     base_url= "https://ww5.7movierulz.tc/"
     url = base_url+movie
     response = request('GET',url)
@@ -24,7 +25,7 @@ def get_torrent(movie_name):
             torrent_resolution_ls.append(torrent.text.strip())
 
     torrent_info = dict(zip(torrent_size_ls,torrent_resolution_ls))
-    keyorder = ['8K','4K', '4k 2160p','4K SDR','1080p','1440p','720p', '480p', '320p','240p','144p']
+    keyorder = ['8K','4K', '4K 2160p','4K SDR','1080p','1440p','720p', '480p', '320p','240p','144p']
     torrent_info = sorted(torrent_info.items(), key=lambda i:keyorder.index(i[1]))
 
     for link in container:
--- telebot.py
+++ telebot.py
@@ -4,13 +4,9 @@ from requests import request
 from bs4 import BeautifulSoup as soup
 import time
 import pickle
-from telegram import ParseMode
-import wget
 
 import re
-import libtorrent
 import time
-import sys
 import os.path
 import difflib 
 from collections import OrderedDict
@@ -18,7 +14,6 @@ import itertools
 
 #local imports
 from latest_url import get_latest_movirulz_url
-from mg2tor import mag2tor
 from get_torrent import get_torrent
 
 
@@ -39,30 +34,32 @@ try:
             magnets = pickle.load(f)
         with open('torrents.pkl', 'rb') as ff:
             torrents = pickle.load(ff)
+        try:   
+            title_of_movie = movie_name_to_download+"_"+torrents[user_quality_choiceee-1][1]+"_"+torrents[user_quality_choiceee-1][0]
+                
+            magnet_link = magnets[torrents[user_quality_choiceee-1][0]]
+            with open(f'torrents/{title_of_movie}.txt','w') as fp:
+                fp.write(magnet_link)
             
-        title_of_movie = movie_name_to_download+"_"+torrents[user_quality_choiceee-1][1]+"_"+torrents[user_quality_choiceee-1][0]
-            
-        magnet_link = magnets[torrents[user_quality_choiceee-1][0]]
-        with open(f'torrents/{title_of_movie}.txt','w') as fp:
-            fp.write(magnet_link)
-        
-        bot.send_message(message.chat.id,"Getting requested magnet link....")
-        time.sleep(0.2)
-        bot.send_message(message.chat.id,"Obtaining Metadata...")
-        time.sleep(0.2)
-        bot.send_message(message.chat.id,"Calculating seeds and peers....")
-        bot.send_message(message.chat.id,"Here is Your Requested Magnet Link, Enjoy your movie...")
+            bot.send_message(message.chat.id,"Getting requested magnet link....")
+            time.sleep(0.2)
+            bot.send_message(message.chat.id,"Obtaining Metadata...")
+            time.sleep(0.2)
+            bot.send_message(message.chat.id,"Calculating seeds and peers....")
+            bot.send_message(message.chat.id,"Here is Your Requested Magnet Link, Enjoy your movie...")
 
-        torrent_hash = magnet_link[20:60].upper()
-        torrent_download_link_= f"https://itorrents.org/torrent/{torrent_hash}.torrent"
+            torrent_hash = magnet_link[20:60].upper()
+            torrent_download_link_= f"https://itorrents.org/torrent/{torrent_hash}.torrent"
 
 
-        torrent_response = requests.get(torrent_download_link_, stream=True, headers={'User-agent': 'Mozilla/5.0'})
-        open(f'torrents/{title_of_movie}.torrent',"wb").write(torrent_response.content)
-        torrent_file = open(f'torrents/{title_of_movie}.torrent', 'rb')
-        bot.send_document(message.from_user.id, torrent_file)
-        bot.send_message(message.chat.id,"If the above torrent fails to add , paste this magnet url in your torrent client")
-        bot.send_message(message.chat.id,magnet_link)
+            torrent_response = requests.get(torrent_download_link_, stream=True, headers={'User-agent': 'Mozilla/5.0'})
+            open(f'torrents/{title_of_movie}.torrent',"wb").write(torrent_response.content)
+            torrent_file = open(f'torrents/{title_of_movie}.torrent', 'rb')
+            bot.send_document(message.from_user.id, torrent_file)
+            bot.send_message(message.chat.id,"If the above torrent fails to add , paste this magnet url in your torrent client")
+            bot.send_message(message.chat.id,magnet_link)
+        except:
+            bot.send_message(message.chat.id,"You Entered Wrong Option Mate, Try Again from /Download_Movie")
 
       
       
@@ -70,30 +67,32 @@ try:
         user_movie_choiceee = user_movie_choice.text
         user_movie_choicee = int(user_movie_choiceee)
         
-        with open('trimmed_movie_title_ls.pkl', 'rb') as f:
-            trimmed_movie_title_ls = pickle.load(f)
+        with open('close_results_ls.pkl', 'rb') as f:
+            close_results_ls = pickle.load(f)
+        try:
+            global movie_name_to_download
+            movie_name_to_download = close_results_ls[user_movie_choicee-1]
+            
+            torrents,magnets = get_torrent(movie_name_to_download)
+            
+            with open('magnets.pkl', 'wb') as f:
+                pickle.dump(magnets, f)
+            with open('torrents.pkl', 'wb') as ff:
+                pickle.dump(torrents, ff)
+            
+            quality_size="";c=1
+            for ___ in torrents:
+                quality_size+=f"{c}.{___[1]} <---> {___[0]}\n"
+                c+=1
+            
+            print_2 = f"I found the following qualities for {movie_name_to_download}"
+            bot.send_message(message.chat.id,print_2)
+            bot.send_message(message.chat.id,quality_size)
+            user_quality_choice = bot.send_message(message.chat.id,f"Enter Number of your desired quality from 1-{c-1}: ")
         
-        global movie_name_to_download
-        movie_name_to_download = trimmed_movie_title_ls[user_movie_choicee-1]
-        
-        torrents,magnets = get_torrent(movie_name_to_download)
-        
-        with open('magnets.pkl', 'wb') as f:
-            pickle.dump(magnets, f)
-        with open('torrents.pkl', 'wb') as ff:
-            pickle.dump(torrents, ff)
-        
-        quality_size="";c=1
-        for ___ in torrents:
-            quality_size+=f"{c}.{___[1]} <---> {___[0]}\n"
-            c+=1
-        
-        print_2 = f"I found the following qualities for {movie_name.text}"
-        bot.send_message(message.chat.id,print_2)
-        bot.send_message(message.chat.id,quality_size)
-        user_quality_choice = bot.send_message(message.chat.id,f"Enter Number of your desired quality from 1-{c-1}: ")
-        
-        bot.register_next_step_handler(user_quality_choice, downmovie_util_3)
+            bot.register_next_step_handler(user_quality_choice, downmovie_util_3)
+        except:
+            bot.send_message(message.chat.id,"You Entered Wrong Option Mate, Try Again from /Download_Movie")
     
       def downmovie_util(movie_name):
         movierulz_url = get_latest_movirulz_url('movierulz')+'?s='
@@ -141,11 +140,11 @@ try:
                 movie_name_string = f"{count}.{close_results[__]}"
                 count+=1
                 bot.send_message(message.chat.id,str(movie_name_string))
-                
-            with open('trimmed_movie_title_ls.pkl', 'wb') as f:
-                pickle.dump(trimmed_movie_title_ls, f)
-                
+            close_results_ls = [*close_results.values()]
             
+            with open('close_results_ls.pkl', 'wb') as f:
+                pickle.dump(close_results_ls, f)
+                
             user_movie_choice = bot.send_message(message.chat.id,f"Enter Number of your desired movie from 1-{count-1}: ")
             
             bot.register_next_step_handler(user_movie_choice, downmovie_util_2)
